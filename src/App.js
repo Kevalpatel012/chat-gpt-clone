af40028073805059a1bbdb19687b044086470334
Keval Patel <kp005919@ohio.edu>
2023-06-11 14:41:44 -0400

    fixed green button and added logo for history

--- src/App.js
+++ src/App.js
@@ -1,82 +1,81 @@
-
-
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 
 const App = () => {
-  const[ value, setValue] = useState(null)
-  const [ message, setMessage] = useState(null)
-  const [previousChats, setPreviousChats] = useState([])
-  const [currentTitle, setCurrentTitle] = useState(null)
-
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
 
   const createNewChat = () => {
-    setMessage(null)
-    setValue("")
-    setCurrentTitle(null)
-  }
+    setMessage(null);
+    setValue('');
+    setCurrentTitle(null);
+  };
 
-  const handleClick = (uniqueTitles) => {
-      setCurrentTitle(uniqueTitles)
-      setMessage(null)
-      setValue("")
-  }
-
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue('');
+  };
 
   const getMessages = async () => {
     const options = {
-      method: "POST",
-      body : JSON.stringify({
-        message: value
+      method: 'POST',
+      body: JSON.stringify({
+        message: value,
       }),
       headers: {
-        "Content-Type": "application/json"
-      }
-    }
-    try{
-      const response = await fetch('http://localhost:8000/completions', options)
-      const data = await response.json()
-      setMessage(data.choices[0].message)
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const response = await fetch('http://localhost:8000/completions', options);
+      const data = await response.json();
+      setMessage(data.choices[0].message);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-useEffect(() => {
-    console.log(currentTitle, value, message)
-    if(!currentTitle && value && message){
-      setCurrentTitle(value)
-    } 
-    if(currentTitle && value && message){
-      setPreviousChats(prevChats => (
-        [...prevChats, 
-          {
-            title: currentTitle, 
-            role: "user",
-            content: value
-          },
-          {
-            title: currentTitle,
-            role: message.role,
-            content: message.content
-          }
-        ]
-      ))
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
     }
-}, [message, currentTitle])
+    if (currentTitle && value && message) {
+      setPreviousChats((prevChats) => [
+        ...prevChats,
+        {
+          title: currentTitle,
+          role: 'user',
+          content: value,
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        },
+      ]);
+    }
+  }, [message, currentTitle]);
 
-    console.log(previousChats)
+  console.log(previousChats);
 
-    const currentChat = previousChats.filter(previousChats => previousChats.title === currentTitle)
-    const uniqueTitles = Array.from(new Set(previousChats.map(previousChats => previousChats.title)))
-    console.log(uniqueTitles)
-
+  const currentChat = previousChats.filter((chat) => chat.title === currentTitle);
+  const uniqueTitles = Array.from(new Set(previousChats.map((chat) => chat.title)));
+  console.log(uniqueTitles);
 
   return (
     <div className="app">
       <section className="side-bar">
         <button onClick={createNewChat}>+ New Chat</button>
         <ul className="history">
-          {uniqueTitles?.map((uniqueTitles, index) => <li key = {index} onClick={() => handleClick(uniqueTitles)}>{uniqueTitles}</li>)}
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              <span className="chat-symbol">§ </span>
+              {uniqueTitle}
+            </li>
+          ))}
         </ul>
         <nav>
           <p>Made By Keval</p>
@@ -85,23 +84,28 @@ useEffect(() => {
       <section className="main">
         {!currentTitle && <h1>KevalGPT</h1>}
         <ul className="feed">
-            {currentChat?.map((chatMessage, index) => <li key={index}>
-                <p className = "role"> {chatMessage.role}</p>
-                <p>{chatMessage.content}</p>
-            </li>)}
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>
+          ))}
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input value = {value} onChange={(e) => setValue(e.target.value)}/>
-            <div id="submit" onClick={getMessages}>➢</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>
+              ➢
+            </div>
           </div>
           <p className="info">
-            Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 24 Version
+            Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May
+            24 Version
           </p>
         </div>
       </section>
     </div>
   );
-}
+};
 
 export default App;
