12aa402677ef607be2e144d0b5a3606ef0f76fb1
Keval Patel <kp005919@ohio.edu>
2023-06-20 13:39:09 -0400

    got deployed but noe the api is acting up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 
-// Create a root for concurrent mode rendering
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
-// Render the App component inside a StrictMode for additional checks
 root.render(
   <React.StrictMode>
     <App />
