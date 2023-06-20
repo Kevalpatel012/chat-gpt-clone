8479fa9cc89b8c4e6bff44b709d59e61f11fc765
Keval Patel <kp005919@ohio.edu>
2023-06-20 12:55:33 -0400

    another server.js to try to deploy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const fetch = require('node-fetch');
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 // Middleware
 app.use(express.json());
@@ -26,26 +26,15 @@ app.post('/completions', async (req, res) => {
       max_tokens: 100,
     }),
   };
+
   try {
     const response = await fetch('https://api.openai.com/v1/chat/completions', options);
     const data = await response.json();
     res.send(data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Something went wrong.' });
+    res.status(500).json({ error: 'An error occurred' });
   }
-});
-
-// Example route handler
-app.get('/', (req, res) => {
-  // Handle the '/' route request
-  res.send('Welcome to my application!');
-});
-
-app.get('/api/data', (req, res) => {
-  // Handle the '/api/data' route request
-  // Perform necessary operations and send a response
-  res.json({ message: 'Here is some data from the API.' });
 });
 
 // Start the server
