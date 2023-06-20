3287ab6a224b88c5dd73e3d5db62fb905eddc057
Keval Patel <kp005919@ohio.edu>
2023-06-20 12:39:25 -0400

    trying to deplay to heroku

--- server.js
+++ server.js
@@ -1,38 +1,13 @@
-const PORT = 8000;
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-
 const app = express();
+const port = process.env.PORT || 8000;
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+// Configure your routes and middleware here
 
-const API_KEY = process.env.API_KEY;
-
-// API Endpoint for chat completions
-app.post('/completions', async (req, res) => {
-  const options = {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: req.body.message }],
-      max_tokens: 100,
-    }),
-  };
-  try {
-    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
-    const data = await response.json();
-    res.send(data);
-  } catch (error) {
-    console.error(error);
-  }
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
 
-// Start the server
-app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT));
+app.listen(port, () => {
+  console.log(`Your server is running on PORT ${port}`);
+});
